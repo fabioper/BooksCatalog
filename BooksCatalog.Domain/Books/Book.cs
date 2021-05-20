7c93004702ead08c50e5499d1a97cb270b93635e
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-05-20 01:22:45 -0300

    Rename field

--- BooksCatalog.Domain/Books/Book.cs
+++ BooksCatalog.Domain/Books/Book.cs
@@ -8,6 +8,6 @@ namespace BooksCatalog.Core.Books
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Description { get; set; }
-        public string ISBN { get; set; }
+        public string Isbn { get; set; }
     }
 }
