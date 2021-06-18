fbcfc2c1fe8528f680ac0de599ffd433caddca53
Fábio Pereira <fabiopereira.fps@gmail.com>
2021-06-17 21:41:21 -0300

    Add book cover uri to response

--- BooksCatalog.Api/Models/Responses/BookResponse.cs
+++ BooksCatalog.Api/Models/Responses/BookResponse.cs
@@ -10,6 +10,7 @@ namespace BooksCatalog.Api.Models.Responses
         public DateTime ReleaseDate { get; set; }
         public string Description { get; set; }
         public string Isbn { get; set; }
+        public string CoverUri { get; set; }
         public List<AuthorResponse> Authors { get; set; }
         public List<GenreResponse> Genres { get; set; }
         public List<PublisherResponse> Publishers { get; set; }
